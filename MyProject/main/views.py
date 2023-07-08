95fcb2596d6907687b4505c3db662524560efa6a
dzmitryshpak <damon_l@inbox.ru>
2023-07-08 14:22:47 +0300

    start_files

---
+++ MyProject/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    data = {
+        'title': 'Про нас',
+    }
+    return render(request, 'main/about.html', data)
